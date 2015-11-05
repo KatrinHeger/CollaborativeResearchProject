6f446e2faecb7deb65ec891b8bb4296b437351a6
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-05 20:13:54 +0100

    Adding the rio package to download the data

--- Code_Collab_Research_Project.R
+++ Code_Collab_Research_Project.R
@@ -9,19 +9,20 @@
 # Insert code here to set working directory
 
 # In order to pursue our analysis, two packages are necessary:
-# (1) devtools (to easily install the "repmis" package)
-# (2) repmis
+# (1) rio
+# ??? additional packages here
 # Information about the packages being used in this project
-# can be found in our Citations
+# can be found in our BibTeX file
 # If users would like to check which packages are already
-# installed, please use the following code: "installed.packages()"
+# installed, you can use the following code: "installed.packages()"
 
 # Starting the repmis package
-library(repmis)
+library("rio")
 
 # Loading the data (csv file) from our Github repository
 # using the repmis package
 # SHA-1 hash of the downloaded data file is:
 # aec313662cd7e1e5fa8b205c5c1180d542cee91a
-URL <- paste0('https://raw.githubusercontent.com/KatrinHeger/','CollaborativeResearchProject/master/Datasets/CIMI.csv')
-main <- repmis::source_data(URL)
+library("rio")
+dataset1 <- rio::import("https://raw.githubusercontent.com/KatrinHeger/CollaborativeResearchProject/master/Datasets/CIMI.csv")
+View(dataset1)
