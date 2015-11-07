6b0bedfbf9493b613c403c9c67af45a684e31aeb
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-07 15:30:28 +0100

    Adding note on retrieving the CIMI dataset with the DVN package

--- Code_Collab_Research_Project.R
+++ Code_Collab_Research_Project.R
@@ -24,5 +24,15 @@ library("rio")
 # Loading the CIMI dataset (csv file) from our Github repository
 # SHA-1 hash of the downloaded data file is:
 # aec313662cd7e1e5fa8b205c5c1180d542cee91a
+# The file was retrieved from
+# https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/BTKZEQ
+# on November 1, 2015.
 dataset_CIMI <- rio::import("https://raw.githubusercontent.com/KatrinHeger/CollaborativeResearchProject/master/Datasets/CIMI.csv")
 View(dataset_CIMI)
+
+# Note on the CIMI dataset: It was not possible so far to retrieve the necessary
+# fileid in order to find out if the dataset we would like to use can be downloaded
+# with a package called "DVN" (https://cran.r-project.org/web/packages/dvn/index.html)
+# Therefore, the authors of the dataset have been contacted on November 7, 2015.
+# We would like to keep the option of directly accessing the dataset directly
+# from Dataverse into R Studio at a later point.
