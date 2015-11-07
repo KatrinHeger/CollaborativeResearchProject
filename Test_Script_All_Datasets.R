4b1409ff85d10471773d5f11f6610605b95b77cf
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-07 16:56:31 +0100

    Uploading import_script for UCDP_CT

--- Test_Script_All_Datasets.R
+++ Test_Script_All_Datasets.R
@@ -7,7 +7,6 @@ library("gdata")
 
 # Loading the CIMI dataset (csv file) from our Github repository
 dataset_CIMI <- rio::import("https://raw.githubusercontent.com/KatrinHeger/CollaborativeResearchProject/master/Datasets/CIMI.csv")
-View(dataset_CIMI)
 
 # Loading the UCDP External Support dataset
 UCDP_ES_xls = "http://www.pcr.uu.se/digitalAssets/159/159834_1external_support_compact_dataset_1.00_20110325-1.xls"
