3e2bad732670a4daa029121bba1d84a9a8bf16fc
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-07 17:18:44 +0100

    Changing the UCDP_CT_data import to the correct sheet number

--- Importing_UCDP_CT.R
+++ Importing_UCDP_CT.R
@@ -1,8 +1,8 @@
 # Loading the gdata package
 library("gdata")
 
-# Downlading UCDP_ES_xls dataset
+# Downlading UCDP_CT_xls dataset
 UCDP_CT_xls = "http://www.pcr.uu.se/digitalAssets/124/124924_1ucdp_conflict_termination_2010_dyad.xls"
 
 # Importing sheet 4 from UCDP_CT_xls datatset into R Studio
-UCDP_CT_data = gdata::read.xls(UCDP_CT_xls, sheet = 1)
+UCDP_CT_data = gdata::read.xls(UCDP_CT_xls, sheet = 4)
