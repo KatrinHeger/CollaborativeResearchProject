379822ace0e7bd0f6fff31a2ac41b644670f37b9
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-07 16:54:32 +0100

    Updates to UCDP Test Scripts

--- Test_Script_UCDP_ES.R
+++ Test_Script_UCDP_ES.R
@@ -2,4 +2,3 @@ library("gdata")
 UCDP_ES_xls = "http://www.pcr.uu.se/digitalAssets/159/159834_1external_support_compact_dataset_1.00_20110325-1.xls"
 gdata::sheetCount(UCDP_ES_xls)
 gdata::sheetNames(UCDP_ES_xls)
-View(UCDP_ES_xls)
