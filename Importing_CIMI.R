b2d1f3fadf8ccc6d2d665e9622587dd359c9c331
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-07 17:13:38 +0100

    Uploading the Importing_CIMI dataset

---
+++ Importing_CIMI.R
@@ -0,0 +1,5 @@
+# Loading the rio package
+library("rio")
+
+# Loading and importing the CIMI dataset (csv file) from our Github repository
+dataset_CIMI <- rio::import("https://raw.githubusercontent.com/KatrinHeger/CollaborativeResearchProject/master/Datasets/CIMI.csv")
