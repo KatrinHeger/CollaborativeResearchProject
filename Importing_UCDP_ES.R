5f53cdf07f530c267535262cce43264288aa356f
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-09 10:49:45 +0100

    Uploading UCDP_ES dataset for tests

--- Importing_UCDP_ES.R
+++ Importing_UCDP_ES.R
@@ -1,8 +1,9 @@
-# Loading the gdata package
+# Loading the rio and gdata package
+library("rio")
 library("gdata")
 
 # Downlading UCDP_ES_xls dataset
 UCDP_ES_xls = "http://www.pcr.uu.se/digitalAssets/159/159834_1external_support_compact_dataset_1.00_20110325-1.xls"
 
 # Importing UCDP_ES_xls dataset
-UCDP_ES_data = gdata::read.xls(UCDP_ES_xls, sheet = 1)
+main <- rio:: import("UCDP_ES_xls")
