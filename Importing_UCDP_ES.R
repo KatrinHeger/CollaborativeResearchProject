7835f9cff2047d8ca91d053d1960739f48e01b9a
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-09 11:54:28 +0100

    Linking dataset in folder

--- Importing_UCDP_ES.R
+++ Importing_UCDP_ES.R
@@ -3,6 +3,5 @@ library("rio")
 
 # Importing the UCDP_ES dataset from Github repository
 # Note:
-# Downloading the datatset directly from the website
-# will be added later
-UCDP_ES <- rio::import("159834_1external_support_compact_dataset_1.00_20110325-1.xls")
+# Downloading the datatset directly from the website will be added later
+UCDP_ES <- rio::import("~/GitHub/CollaborativeResearchProject/Datasets/UCDP_ExternalSupportDataset.xls")
