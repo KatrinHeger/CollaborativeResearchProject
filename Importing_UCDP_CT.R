f66c69e0d86f234a0a3ea2329a93c4884a414c13
mr-r0b0t <mr-r0b0t@users.noreply.github.com>
2015-11-10 10:08:43 +0100

    Delete Importing_UCDP_CT.R

--- Importing_UCDP_CT.R
+++
@@ -1,8 +0,0 @@
-# Loading the gdata package
-library("gdata")
-
-# Downlading UCDP_CT_xls dataset
-UCDP_CT_xls = "http://www.pcr.uu.se/digitalAssets/124/124924_1ucdp_conflict_termination_2010_dyad.xls"
-
-# Importing sheet 4 from UCDP_CT_xls datatset into R Studio
-UCDP_CT = gdata::read.xls(UCDP_CT_xls, sheet = 4)
