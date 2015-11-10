00ea49c601d12353260a6052ea17f9a62e407568
mr-r0b0t <mr-r0b0t@users.noreply.github.com>
2015-11-10 10:08:28 +0100

    Delete Importing_CIMI.R

--- Importing_CIMI.R
+++
@@ -1,5 +0,0 @@
-# Loading the rio package
-library("rio")
-
-# Loading and importing the CIMI dataset (csv file) from our Github repository
-CIMI <- rio::import("https://raw.githubusercontent.com/KatrinHeger/CollaborativeResearchProject/master/Datasets/CIMI.csv")
