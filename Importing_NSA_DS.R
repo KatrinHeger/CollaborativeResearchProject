886d0869902b3a3602010a8b106a1d81a7b0fc83
mr-r0b0t <mr-r0b0t@users.noreply.github.com>
2015-11-10 10:08:36 +0100

    Delete Importing_NSA_DS.R

--- Importing_NSA_DS.R
+++
@@ -1,5 +0,0 @@
-# Loading the rio package
-library("rio")
-
-# Downloading and importing the NSA_DS
-dyads <- rio:: import("http://privatewww.essex.ac.uk/~ksg/data/nsa_v3.4_21November2013.asc", format = "tsv")
