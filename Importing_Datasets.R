173a8232e882a3eccd77b5fd2fd8f900b0334b8a
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-10 10:11:45 +0100

    Changing names of datasets

--- Importing_Datasets.R
+++ Importing_Datasets.R
@@ -3,14 +3,14 @@ library("rio")
 library("gdata")
 
 # Importing the CIMI_Dataset
-CIMI <- rio::import("https://raw.githubusercontent.com/KatrinHeger/CollaborativeResearchProject/master/Datasets/CIMI.csv")
+CIMI_Dataset <- rio::import("https://raw.githubusercontent.com/KatrinHeger/CollaborativeResearchProject/master/Datasets/CIMI.csv")
 
 # Importing the UCDP_ES_Dataset
-UCDP_ES <- rio::import("~/GitHub/CollaborativeResearchProject/Datasets/UCDP_ExternalSupportDataset.xls")
+UCDP_ES_Dataset <- rio::import("~/GitHub/CollaborativeResearchProject/Datasets/UCDP_ExternalSupportDataset.xls")
 
 # Importing the UCDP_CT_Dataset
 UCDP_CT_xls = "http://www.pcr.uu.se/digitalAssets/124/124924_1ucdp_conflict_termination_2010_dyad.xls"
-UCDP_CT = gdata::read.xls(UCDP_CT_xls, sheet = 4)
+UCDP_CT_Dataset = gdata::read.xls(UCDP_CT_xls, sheet = 4)
 
 # Importing the NSA_Dataset
-dyads <- rio:: import("http://privatewww.essex.ac.uk/~ksg/data/nsa_v3.4_21November2013.asc", format = "tsv")
+NSA_Dataset <- rio:: import("http://privatewww.essex.ac.uk/~ksg/data/nsa_v3.4_21November2013.asc", format = "tsv")
