bab9e5ddaf4d431651f7d4303ec73735f1f21367
mr-r0b0t <mr-r0b0t@users.noreply.github.com>
2015-11-10 10:08:52 +0100

    Delete Importing_UCDP_ES.R

--- Importing_UCDP_ES.R
+++
@@ -1,7 +0,0 @@
-# Loading the rio and gdata package
-library("rio")
-
-# Importing the UCDP_ES dataset from Github repository
-# Note:
-# Downloading the datatset directly from the website will be added later
-UCDP_ES <- rio::import("~/GitHub/CollaborativeResearchProject/Datasets/UCDP_ExternalSupportDataset.xls")
