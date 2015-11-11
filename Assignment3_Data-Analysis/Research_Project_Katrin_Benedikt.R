0fedac2f214f8c222185d0539a6cfda0e803b565
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-11 11:37:35 +0100

    Adding the missing code to our main file

--- Assignment3_Data-Analysis/Research_Project_Katrin_Benedikt.R
+++ Assignment3_Data-Analysis/Research_Project_Katrin_Benedikt.R
@@ -6,26 +6,53 @@
 # For more information please read the README file               #
 ##################################################################
 
+# Cleaning the environment in R Studio
+rm(list=ls())
+
 # Setting the working directory
-# Include code here at the end (probably from the repmis package
-# and the slides from Lecture 6, 9 October 2015)
+# getwd()
+# setwd(add at end) !!!
 
 # In order to pursue our analysis, the follogwing packages are necessary:
 # (1) rio
+# (2) gdata
+# (3) mlogit
 # ??? additional packages here
 # Information about the packages being used in this project
-# can be found in our BibTeX file
+# can be found in our BibTeX file.
 # If users would like to check which packages are already
 # installed, use the following code: "installed.packages()"
 
 # Starting the rio package
 library("rio")
+library("gdata")
+library("mlogit")
 
-# Loading the data (csv file) from our Github repository
-# using the repmis package
-# SHA-1 hash of the downloaded data file is:
+# Loading the CIMI_Dataset from our Github repository
+# Note:
+# The only package (as fas as we know) that can directly access the
+# API of the Harvard Dataverse Network is a package called "DVN".
+# Unfortunately, it hasn't been updated yet to work with the current
+# Harvard Dataverse version 4.0. Hence, we had to download it by hand.
+# Source of issue of DVN package: https://github.com/ropensci/dvn/issues/23
+# The file was retrieved on November 1, 2015 from
+# https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/BTKZEQ
+# SHA-1 hash of the downloaded data file:
 # aec313662cd7e1e5fa8b205c5c1180d542cee91a
-library("rio")
-dataset1 <- rio::import("https://raw.githubusercontent.com/KatrinHeger/CollaborativeResearchProject/master/Datasets/CIMI.csv")
-View(dataset1)
+CIMI_Dataset <- rio::import("https://raw.githubusercontent.com/KatrinHeger/CollaborativeResearchProject/master/Datasets/CIMI.csv")
 
+# Importing the UCDP_ES_Dataset
+UCDP_ES_Dataset <- rio::import("~/GitHub/CollaborativeResearchProject/Datasets/UCDP_ExternalSupportDataset.xls")
+
+# Importing the UCDP_CT_Dataset
+UCDP_CT_xls = "http://www.pcr.uu.se/digitalAssets/124/124924_1ucdp_conflict_termination_2010_dyad.xls"
+UCDP_CT_Dataset = gdata::read.xls(UCDP_CT_xls, sheet = 4)
+
+# Note on UCDP datasets: 
+# There is an R package called "UCDPtools" (https://github.com/tlscherer/UCDPtools),
+# which is supposed to directly import datasets from UCDP into R Studio. Unfortunately,
+# the current version of R Studio (3.2.2) is not supported. As we think that this tool
+# might be very helpful for other researchers as well, the author has been contacted.
+
+# Importing the NSA_Dataset
+NSA_Dataset <- rio::import("http://privatewww.essex.ac.uk/~ksg/data/nsa_v3.4_21November2013.asc", format = "tsv")
