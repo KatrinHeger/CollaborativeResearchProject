11ccd4391fadfd4f22827d9453578b0d95d88df9
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-11 22:13:51 +0100

    Uploading TEMPORARY file to check for complete cases

---
+++ Assignment3_Data-Analysis/Checking_Complete_Cases.R
@@ -0,0 +1,5 @@
+#Check for complete cases by dropping all missing (NA) values
+Merged_Dataset_1_Only_Complete_2 <- na.omit(Merged_Dataset_1)
+
+# View new subset to check complete cases
+View(Merged_Dataset_1_Only_Complete_2)
