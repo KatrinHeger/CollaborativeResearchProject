a30327d46cb965cf0d75cff59d62150e41c5fae8
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-13 00:14:23 +0100

    Adding some test code

--- Assignment3_Data-Analysis/Descriptive_Statistics.R
+++ Assignment3_Data-Analysis/Descriptive_Statistics.R
@@ -2,10 +2,21 @@
 # Descriptive Statistics  #
 ###########################
 
-# Summary Statistics
+# Summary Statistics (List min., 1st qu., median, mean, 3rd qu. and max.)
+summary(Dataset_Model_1)
 
 # Histogram of dependent variable Conflict Outcomes
 
 # Histogram of third-party support for rebels
 
 # Histogram of third-party supoprt for government
+
+table(Dataset_Model_1$outcome_d)
+
+table(Dataset_Model_1$fightcaphigh)
+
+table(Dataset_Model_1$outcome_d, Dataset_Model_1$fightcaphigh)
+
+library("Hmisc")
+
+describe(Dataset_Model_1$rebestimate)
