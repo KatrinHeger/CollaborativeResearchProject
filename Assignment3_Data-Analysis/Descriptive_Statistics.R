9db733cbe390cb554a3edeb87d2db0bab1216cfa
KatrinHeger <k.heger@mpp.hertie-school.org>
2015-11-12 20:33:46 +0100

    Update Descriptive Statistics

---
+++ Assignment3_Data-Analysis/Descriptive_Statistics.R
@@ -0,0 +1,11 @@
+###########################
+# Descriptive Statistics  #
+###########################
+
+# Summary Statistics
+
+# Histogram of dependent variable Conflict Outcomes
+
+# Histogram of third-party support for rebels
+
+# Histogram of third-party supoprt for government
