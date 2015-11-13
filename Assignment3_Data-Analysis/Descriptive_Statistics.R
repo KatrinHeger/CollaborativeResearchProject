80efe148aad5275aa3e7bf6c6fabeee77a682e39
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-13 17:00:15 +0100

    Finalizing Descriptive_Statistics with new names

--- Assignment3_Data-Analysis/Descriptive_Statistics.R
+++ Assignment3_Data-Analysis/Descriptive_Statistics.R
@@ -2,68 +2,59 @@
 # Descriptive Statistics  #
 ###########################
 
-# Loading Hmisc package
+# Load Hmisc and ggplot2 packages
 library("Hmisc")
+library("ggplot2")
 
-# Describing the whole data frame
-dim(Model_1_Dataset) # Our dataset currently entails 497 rows and 26 columns/variables
+# Describe the whole data frame
+dim(Dataset_1) # Our dataset currently entails 497 rows and 23 columns/variables
 
 ##################################
 # Conflict Outcomes              #
 ##################################
 
-# Displaying the distribution of all conflict outcomes ("outcome_d"),
+# Show the distribution of all conflict outcomes ("outcome_d"),
 # including the percentage distribution (last line out print).
 # Coding of "outcome_d":
 # 0: low activity
 # 1: rebel victory
 # 2: settlement
 # 3: government victory
-describe(Model_1_Dataset$outcome_d)
+describe(Dataset_1$conflict_outcome)
 
 # Create histogram of conflict outcomes ("outcome_d")
 # Note:
 # Unfortunately we still can't create the histogram when the code is not on one single line.
 # Therefore, all following histograms will be written in one line.
 # See https://github.com/HertieDataScience/SyllabusAndLectures/issues/36
-hist(Model_1_Dataset$outcome_d, main = "Frequency of Conflict Outcomes", xlab = "Conflict Outcomes", las = 1, breaks = seq(-.5,3.5,1), col = "lightgreen")
-
-# Create legend for histogram with outcome types
-legend("topright", title="Outcome Types", c("0: Low Activity", "1: Rebel Victory","2: Settlement","4: Govt. Victory"), cex = 0.4)
+qplot(Dataset_1$conflict_outcome, geom = "histogram", binwidth = .5, main = "Frequency of Rebel Support Types", xlab = "Support Types", ylab = "Frequency", fill = I("lightblue"))
 
 ##################################
 # Types of Government Support    #
 ##################################
 
-# Displaying the distribution of all types of government support ("gtypesup_cat"),
+# Display the distribution of all types of government support ("gtypesup_cat"),
 # including the percentage distribution (last line out print).
 # Coding of "gtypesup_cat":
 # 1: troops
 # 2: military
 # 3: non-military
-describe(Model_1_Dataset$gtypesup_cat)
+describe(Dataset_1$gtypesup_cat)
 
 # Histogram of third-party support types for government
-hist(Model_1_Dataset$gtypesup_cat, main = "Frequency of Government Support Types", xlab = "Government Support Types", las = 1, breaks = seq(.5,3.5,1), col = "lightblue")
-
-# Create legend for histogram with support types for government
-legend("topright", title="Support Types", c("1: Troops","2: Military","3: Military"), cex = 0.4)
+qplot(Dataset_1$gtypesup_cat, geom = "histogram", binwidth = .5, main = "Frequency of Government Support Types", xlab = "Support Types", ylab = "Frequency", fill = I("lightgreen"))
 
 ##################################
 # Types of Rebel Support         #
 ##################################
 
-# Displaying the distribution of all types of rebel support ("rtypesup_cat"),
+# Show the distribution of all types of rebel support ("rtypesup_cat"),
 # including the percentage distribution (last line out print).
 # Coding of "rtypesup_cat":
 # 1: troops
 # 2: military
 # 3: non-military
-describe(Model_1_Dataset$rtypesup_cat)
+describe(Dataset_1$rtypesup_cat)
 
-# !!! Histogram not working !!!
 # Histogram of third-party support types for rebels
-hist(Model_1_Dataset$rtypesup_cat, main = "Frequency of Rebel Support Types", xlab = "Rebel Support Types", las = 1, breaks = seq(.5,3.5,1), col = "lightyellow")
-
-# Create legend for histogram with support types for rebels
-legend("topright", title="Support Types", c("1: Troops","2: Military","3: Military"), cex = 0.4)
+qplot(Dataset_1$rtypesup_cat, geom = "histogram", binwidth = .5, main = "Frequency of Rebel Support Types", xlab = "Support Types", ylab = "Frequency", alpha = I(.9), fill = I("brown"))
