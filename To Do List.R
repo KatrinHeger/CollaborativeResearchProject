cea3de6938a43c3a5dfbe4d8f16f8e150ae9a8a8
mr-r0b0t <benediktabendroth@outlook.com>
2015-11-20 12:28:42 +0100

    Uploading To Do List

---
+++ To Do List.R
@@ -0,0 +1,9 @@
+# To do Research Project
+
+# 1. Update Lit Review
+# 2. Develop concrete hypothesis
+# 3. Further develop statistical model (create a more detailed R script with comments)
+# 4. Create presentation
+
+# Tables
+# Descriptives
